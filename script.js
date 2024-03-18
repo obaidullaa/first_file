44f24d319c75ff88ae8e5c45d59effe61cb828a5
Obaidullah Zeb <120409830+obaidullaa@users.noreply.github.com>
2024-03-18 23:44:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function removeFunction(){
+    document.querySelector("h2").innerHTML = "Stranger";
+    document.querySelector("h2").style.color = "red";
+}
+
+
+function friendFunction(){
+    document.querySelector("h2").innerHTML = "Friends";
+    document.querySelector("h2").style.color = "green";
+}
